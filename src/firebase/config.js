9c31a2d394f64f34b5cf9bbae2511433b770f547
Dhrumil <sdhrumil2605@gmail.com>
2021-01-15 16:10:01 -0500

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: "Your API Key",
+    authDomain: "Your Auth Domain",
+    databaseURL: "Your Database URL",
+    projectId: "Your Project ID",
+    storageBucket: "Your Storage Bucket",
+    messagingSenderId: "Your Messaging Sender ID"
+  };
+  
